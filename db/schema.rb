ad30926d8dae58cb26db1b0cf02e8fcaffdd2c0e
Blair <blairacarroll@gmail.com>
2019-08-30 20:28:16 -0400

    working login, navbar, forms, and updated controller methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_182859) do
+ActiveRecord::Schema.define(version: 2019_04_04_102347) do
 
   create_table "room_messages", force: :cascade do |t|
     t.integer "room_id"
